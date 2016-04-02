6022956aaf99c3973aac62c432e2c0aa23760e49
M.Danish Iqbal <mohammad.danish.iqbal@gmail.com>
2016-04-03 00:09:50 +0500

    copyfile not working

--- spark/src/main/java/org/mdicreatives/Hadoop/HDFSFileOperation.java
+++ spark/src/main/java/org/mdicreatives/Hadoop/HDFSFileOperation.java
@@ -1,68 +1,94 @@
 package org.mdicreatives.Hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.fs.Path;
+
 import java.io.*;
-import java.net.URI;
+//import java.net.URI;
 //import java.lang.String;
 //import java.util.*;
 
 public class HDFSFileOperation {
 	//Class Members
-	public Path workingDir;
-	public Path newFolderPath;
+	
 	public FileSystem hdfs;
-	public Path homeDir;
+	public Path newfolderpath;
+	FsShell shell;
 	
 	//Constructor Start
 	public  HDFSFileOperation() throws IOException
-	{
+	{  
+		// Conf object will read the HDFS configuration parameters from these XML
 		Configuration conf= new Configuration();
 		conf.addResource(new Path("/usr/local/hadoop/conf/core-site.xml"));
 	    conf.addResource(new Path("/usr/local/hadoop/conf/hdfs-site.xml"));
-		//conf.set("fs.defaultFS","hdfs://hadoopmaster:54310");
-		FileSystem hdfs =FileSystem.get(conf);
+		conf.set("fs.defaultFS","hdfs://hadoopmaster:54310");
 		
-		 homeDir=hdfs.getHomeDirectory();
-		System.out.println("Home folder - " +  hdfs.getHomeDirectory());
+		//Creating the FileSystem object.
+		 hdfs =FileSystem.get(conf);
+		 shell=new FsShell(conf);
 			
 	}//Constructor End
+	
+	//gives current Directory
+	
+	
+	//Function Start
+	public String getCurrentDirectory()throws IOException
+	{
+		return newfolderpath.toString();
+	}//Function End
+	
+	public void setpath(Path newpath)throws IOException
+	{
+		Path homepath = hdfs.getHomeDirectory();
+		System.out.println("Homepath-"+homepath);
+		newfolderpath = Path.mergePaths(homepath,newpath);
+	}
 	
 	//function start
 	public void makedir(String dir) throws IOException
 	{
-		//Current Working Directory 
-		Path path= new Path(dir);
-	  // workingDir= hdfs.getWorkingDirectory();
-		//newFolderPath= new Path("/MyDataFolder");
-		//newFolderPath=Path.mergePaths(workingDir, newFolderPath);
 		
-		/*if(hdfs.exists(newFolderPath))
+		Path path= new Path(""+dir);
+	 
+		
+		if(hdfs.exists(path))
 		{
-
 		//Delete existing Directory
-
-		hdfs.delete(newFolderPath, true);
-
-		System.out.println("Existing Folder Deleted.");*/
-
+		System.out.println("Directory Already Exists");
+		hdfs.delete(path, true);
+		System.out.println("Existing Directory Deleted.");
+		}
+		System.out.println("Creating New Directory");
 		hdfs.mkdirs(path);     //Create new Directory
-
-		System.out.println("Folder Created.");
-		//}
+		System.out.println("Directory Created.");
+		
 		
 	}//function end
 	
 	//function start
-	public void copyfilestoHDFS() throws IOException
+	public void copyfilestoHDFS(String filename,String localpath) throws IOException
 	{
-		Path localFilePath = new Path("c://localdata/datafile1.txt");
+		
+		
+	      try {
+	        shell.run(new String[]{"-chmod","-R","777",newfolderpath.toString()});
+	      }
+	     catch (  Exception e) {
+	        System.out.println("Couldnt change the file permissions ");
+	        throw new IOException(e);
+	      }
+		Path localFilePath = new Path(localpath+ "/"+ filename);
+		System.out.println(localFilePath);
 
-		Path hdfsFilePath= new Path(newFolderPath+"/dataFile1.txt");
+		Path hdfsFilePath= new Path(newfolderpath.toString()+"/"+ filename);
+		System.out.println(hdfsFilePath);
 
 		hdfs.copyFromLocalFile(localFilePath, hdfsFilePath);
+		
 
 		System.out.println("File copied from local to HDFS.");
+		hdfs.close();
 		
 	}//function end
 	
--- spark/src/main/java/org/mdicreatives/Hadoop/Runner.java
+++ spark/src/main/java/org/mdicreatives/Hadoop/Runner.java
@@ -1,28 +1,29 @@
 package org.mdicreatives.Hadoop;
 import org.mdicreatives.Hadoop.HDFSFileOperation;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.fs.FileSystem;
 
 import java.io.*;
 
 public class Runner {
-	
+	 
+	public static String mypath= "/DistributedAnalytics";
+	public static String forfile= "";
+	public static String localfile = "/home/mdanish/BigData/Distributed Analytics of Machine Data/Code"; //local file path here
+	public static String filename = "pom.xml"; //filename here
+	 
 	public static void main(String[] args) throws IOException 
 	{
-		Configuration conf= new Configuration();
-		conf.addResource(new Path("/usr/local/hadoop/conf/core-site.xml"));
-	    conf.addResource(new Path("/usr/local/hadoop/conf/hdfs-site.xml"));
-		conf.set("fs.defaultFS","hdfs://hadoopmaster:54310");
-		FileSystem hdfs =FileSystem.get(conf);
-		//Path path = new Path("hdfs://localhost:54310/user/hduser/new");
-		Path path = new Path("/user/hduser/new");
-		hdfs.mkdirs(path);
-
-        hdfs.close();
-		//HDFSFileOperation hd= new HDFSFileOperation();
-		//hd.makedir("hdfs://hadoopmaster:54310/user/hduser/new");
-		System.out.println("Runner Completed");//debugger
+		
+		HDFSFileOperation hd= new HDFSFileOperation();
+		//hd.makedir(mypath);
+		
+		Path newpath= (new Path(mypath));
+		hd.setpath(newpath);
+		//forfile=hd.getCurrentDirectory();
+		hd.copyfilestoHDFS(filename,localfile);
+		
+		System.out.println("Runner Done");
+		
 		
 	}
 
