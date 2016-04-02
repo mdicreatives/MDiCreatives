40adf9cf9241e3f0325bc232a6bb08a8c51f772a
M.Danish Iqbal <mohammad.danish.iqbal@gmail.com>
2016-04-02 19:34:24 +0500

    Runner worked

--- spark/src/main/java/org/mdicreatives/Hadoop/HDFSFileOperation.java
+++ spark/src/main/java/org/mdicreatives/Hadoop/HDFSFileOperation.java
@@ -1,45 +1,55 @@
 package org.mdicreatives.Hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.Path;
 import java.io.*;
+import java.net.URI;
 //import java.lang.String;
 //import java.util.*;
 
 public class HDFSFileOperation {
 	//Class Members
-	private Path workingDir;
-	private Path newFolderPath;
-	private FileSystem hdfs;
+	public Path workingDir;
+	public Path newFolderPath;
+	public FileSystem hdfs;
+	public Path homeDir;
 	
 	//Constructor Start
 	public  HDFSFileOperation() throws IOException
 	{
-		FileSystem hdfs =FileSystem.get(new Configuration());
+		Configuration conf= new Configuration();
+		conf.addResource(new Path("/usr/local/hadoop/conf/core-site.xml"));
+	    conf.addResource(new Path("/usr/local/hadoop/conf/hdfs-site.xml"));
+		//conf.set("fs.defaultFS","hdfs://hadoopmaster:54310");
+		FileSystem hdfs =FileSystem.get(conf);
+		
+		 homeDir=hdfs.getHomeDirectory();
 		System.out.println("Home folder - " +  hdfs.getHomeDirectory());
 			
 	}//Constructor End
 	
 	//function start
-	public void makedir() throws IOException
+	public void makedir(String dir) throws IOException
 	{
 		//Current Working Directory 
-	    workingDir= hdfs.getWorkingDirectory();
-		newFolderPath= new Path("/MyDataFolder");
-		newFolderPath=Path.mergePaths(workingDir, newFolderPath);
+		Path path= new Path(dir);
+	  // workingDir= hdfs.getWorkingDirectory();
+		//newFolderPath= new Path("/MyDataFolder");
+		//newFolderPath=Path.mergePaths(workingDir, newFolderPath);
 		
-		if(hdfs.exists(newFolderPath))
+		/*if(hdfs.exists(newFolderPath))
 		{
 
 		//Delete existing Directory
 
 		hdfs.delete(newFolderPath, true);
 
-		System.out.println("Existing Folder Deleted.");
+		System.out.println("Existing Folder Deleted.");*/
 
-		hdfs.mkdirs(newFolderPath);     //Create new Directory
+		hdfs.mkdirs(path);     //Create new Directory
 
 		System.out.println("Folder Created.");
-		}
+		//}
 		
 	}//function end
 	
--- spark/src/main/java/org/mdicreatives/Hadoop/Runner.java
+++ spark/src/main/java/org/mdicreatives/Hadoop/Runner.java
@@ -1,14 +1,28 @@
 package org.mdicreatives.Hadoop;
 import org.mdicreatives.Hadoop.HDFSFileOperation;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FileSystem;
+
 import java.io.*;
 
 public class Runner {
 	
 	public static void main(String[] args) throws IOException 
 	{
-		
-		HDFSFileOperation hd= new HDFSFileOperation();
-		hd.makedir();
+		Configuration conf= new Configuration();
+		conf.addResource(new Path("/usr/local/hadoop/conf/core-site.xml"));
+	    conf.addResource(new Path("/usr/local/hadoop/conf/hdfs-site.xml"));
+		conf.set("fs.defaultFS","hdfs://hadoopmaster:54310");
+		FileSystem hdfs =FileSystem.get(conf);
+		//Path path = new Path("hdfs://localhost:54310/user/hduser/new");
+		Path path = new Path("/user/hduser/new");
+		hdfs.mkdirs(path);
+
+        hdfs.close();
+		//HDFSFileOperation hd= new HDFSFileOperation();
+		//hd.makedir("hdfs://hadoopmaster:54310/user/hduser/new");
+		System.out.println("Runner Completed");//debugger
 		
 	}
 
